50d1c043876df229051842ece29243c62ab9ded8
tuitenlathanh <92715966+tuitenlathanh@users.noreply.github.com>
2023-06-15 08:44:44 +0700

    'model'

---
+++ 2k1Shop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2k1Shop.Model.Abstract
+{
+   public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        [MaxLength(256)]
+        string CreatedBy { set; get; }
+        DateTime? UpdateDate { set; get; }
+        [MaxLength(256)]
+        string UpdatedBy { set; get; }
+
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+
+         bool Status { set; get; }
+    }
+}
